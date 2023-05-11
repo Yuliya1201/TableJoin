16599650609dadd851f94aad18bae22152e0ff32
user <yulechkaakopyan@yandex.ru>
2023-05-11 17:17:40 +0300

    Проект

---
+++ src/main/java/com/example/tablejoin/TableJoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.tablejoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableJoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableJoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tablejoin/TableJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tablejoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableJoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
